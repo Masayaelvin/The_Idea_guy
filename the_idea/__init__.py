f86f34709fda22859f4eae05e0dbcb5aa24b2b2a
Masaya Elvin <masayaelvin@gmail.com>
2024-05-19 08:29:11 +0300

    sqlite_database

--- the_idea/__init__.py
+++ the_idea/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ELVIN_MASAYA:masaya_pwd@localhost/the_idea_guy'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///the_idea_guy.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
 
 db = SQLAlchemy(app)
--- the_idea/models.py
+++ the_idea/models.py
@@ -1,26 +1,31 @@
 from the_idea import db
-
-class users (db.Model):
+''' this is the table for the users in the database'''
+class users(db.Model):
     id = db.Column(db.String(20), primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(80))
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    
-class projects (db.Model):
+
+'''this is the table for the projects in the database'''
+class projects(db.Model):
     project_id = db.Column(db.String(20), primary_key=True)
     difficulty_level = db.Column(db.String(50), nullable=False)
     title = db.Column(db.String(80))
     description = db.Column(db.String(120))
+    category = db.relationship('categories', backref='projects', lazy=True)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    
-class categories (db.Model):
+
+# This is the table for the categories of projects
+class categories(db.Model):
     category_id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    projects = db.relationship('projects', backref='categories', lazy=True)
 
+# This is the association table for the many-to-many relationship between users and projects
 class userProjects(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     project_id = db.Column(db.Integer, db.ForeignKey('projects.project_id'), primary_key=True)
@@ -28,5 +33,5 @@ class userProjects(db.Model):
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     
     
-if __name__ == '__main__':
-    db.create_all()
+# This line creates the tables in the database  
+create = db.create_all()
