80a6680e21ef67569f65457442858c59d79d410d
Masaya Elvin <masayaelvin@gmail.com>
2024-05-20 22:24:49 +0300

    adding routes for getting projects users and filtered projects

--- the_idea/models.py
+++ the_idea/models.py
@@ -3,48 +3,45 @@ from the_idea import db
 # This is the table for the users in the database
 class Users(db.Model):
     id = db.Column(db.String(20), primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(80))
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
-    # Relationship to user projects
-    user_projects = db.relationship('UserProjects', backref='user', lazy=True)
-    
+    # Relationship to projects
+    projects = db.relationship('Projects', backref='author', lazy=True)
+
     def __repr__(self):
         return f'<Users id={self.id} username={self.username} email={self.email}>'
 
 # This is the table for the projects in the database
 class Projects(db.Model):
     project_id = db.Column(db.String(20), primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
     difficulty_level = db.Column(db.String(50), nullable=False)
-    title = db.Column(db.String(80))
-    description = db.Column(db.String(120))
+    description = db.Column(db.Text, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('categories.category_id'), nullable=False)
+    user_id = db.Column(db.String(20), db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
     # Relationship to categories
     category = db.relationship('Categories', backref='projects', lazy=True)
-    # Relationship to user projects
-    project_users = db.relationship('UserProjects', backref='project', lazy=True)
+
+    def __repr__(self):
+        return f'<Projects project_id={self.project_id} title={self.title}>'
 
 # This is the table for the categories of projects
 class Categories(db.Model):
     category_id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-
-# This is the association table for the many-to-many relationship between users and projects
-class UserProjects(db.Model):
-    user_id = db.Column(db.String(20), db.ForeignKey('users.id'), primary_key=True)
-    project_id = db.Column(db.String(20), db.ForeignKey('projects.project_id'), primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
+    def __repr__(self):
+        return f'<Categories category_id={self.category_id} category_name={self.category_name}>'
 
 # This line creates the tables in the database
-# create = db.create_all()
-
-    
+# db.drop_all()
+# db.create_all()
--- the_idea/routes.py
+++ the_idea/routes.py
@@ -1,7 +1,53 @@
 from the_idea import app, db
-from the_idea.models import Users, Projects, Categories, UserProjects
+from the_idea.models import Users, Projects, Categories
+from flask import jsonify
 
-@app.route('/home')
+@app.route('/', methods = ['GET'])
 def home():
     users = Users.query.all()
-    return ', '.join([str(user.username) for user in users]) 
+    return jsonify([{
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'created_at': user.created_at,
+        'updated_at': user.updated_at
+    } for user in users])
+
+@app.route('/projects', methods = ['GET'])    
+def Projects():
+    projects = Projects.query.all()
+    return jsonify([{
+        'project_id': project.project_id,
+        'title': project.title,
+        'difficulty_level': project.difficulty_level,
+        'description': project.description,
+        'category_id': project.category_id,
+        'user_id': project.user_id,
+        'created_at': project.created_at,
+        'updated_at': project.updated_at
+    } for project in projects])
+
+@app.route('user_projects', methods = ['GET'])
+def user_projects():
+    user_projects = Projects.query.filter_by(user_id = '1').all()
+    return jsonify([{
+        'project_id': project.project_id,
+        'title': project.title,
+        'difficulty_level': project.difficulty_level,
+        'description': project.description,
+        'category_id': project.category_id,
+        'user_id': project.user_id,
+        'created_at': project.created_at,
+        'updated_at': project.updated_at
+    } for project in user_projects])
+    
+
+@app.route('/categories', methods = ['GET'])
+def categories(category):
+    categories = Categories.query.all()
+    for category in categories:
+        return jsonify([{
+            category : [{
+                category.projects
+            }]
+        }])
