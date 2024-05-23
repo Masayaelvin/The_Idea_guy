ae37c8afca9444426dd2e83ea563696329ad63f4
Masaya Elvin <masayaelvin@gmail.com>
2024-05-23 22:52:45 +0300

    password type

--- the_idea/forms.py
+++ the_idea/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, SubmitField, IntegerField, BooleanField, SelectField, TextAreaField, DateField, TimeField)
+from wtforms import (StringField, SubmitField, IntegerField, BooleanField, SelectField, TextAreaField, DateField, PasswordField)
 from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional, ValidationError
 from flask_ckeditor import CKEditorField
 from the_idea.models import Categories
@@ -7,13 +7,13 @@ from the_idea.models import Categories
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = StringField('Password', validators=[DataRequired()])
-    confirm_password = StringField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
     
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
