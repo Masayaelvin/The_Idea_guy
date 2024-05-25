f8184f5ee355dc6bdc97e2944c3e43dda4758004
Masaya Elvin <masayaelvin@gmail.com>
2024-05-25 21:54:15 +0300

    random idea implementation

--- the_idea/routes.py
+++ the_idea/routes.py
@@ -112,8 +112,20 @@ def all_Projects():
     
 @app.route('/random_idea', methods = ['GET'])
 def random_idea():
-    ideas = all_Projects()
-    random_idea = random.choice(ideas)
+    ideas =  Users.query.all()
+    projects = [{
+        'project_id': project.project_id,
+        'title': project.title,
+        'difficulty_level': project.difficulty_level,
+        'description': project.description,
+        'category_id': project.category_id,
+        'user_id': project.user_id,
+        'created_at': project.created_at,
+        'updated_at': project.updated_at
+    } for user in ideas for project in user.projects
+    ]
+
+    random_idea = random.choice(projects)
     return render_template('random_idea.html', random_idea = random_idea, title = 'Random Idea')
 
 @app.route('/user_projects', methods = ['GET'])
